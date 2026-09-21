9fa71ae901be86853fff05279d6b6588c8ca6aa9
seamaneiei51-commits <seamaneiei51@gmail.com>
2026-09-22 00:14:42 +0700

    Delete Key_plus.py

--- Key_plus.py
+++
@@ -1,8 +0,0 @@
-def combine_dicts(d1, d2):
-    result = d1.copy()
-    for key, value in d2.items():
-        if key in result:
-            result[key] += value
-        else:
-            result[key] = value
-    return result
